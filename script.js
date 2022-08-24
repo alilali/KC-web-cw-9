c1abec25999e9d1116283fb9c6e5d22f8a94bc01
علي جعفر العلي <109864451+alilali@users.noreply.github.com>
2022-08-24 19:42:07 +0300

    writting some codes

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // أنشئ المصفوفة تحت هذا الكومنت
+let orders = [
 
+
+]
+orders.push(newOrder)
 
 function addOrder() {
   let name = document.getElementById("name").value;
@@ -8,16 +12,24 @@ function addOrder() {
   let order = document.getElementById("order").value;
   
   // أنشئ كائن newOrder
-  
+  let newOrder = 
+  [
+    name ,
+    id, 
+    phone ,
+    number
+  ]
   // أدرج الكائن إلى المصفوفة
   
   // طبق الخطوة 7 تحت هذا الكومنت
-  
+  load ()
 }
 
-function load() {
+function load( ) {
   let container = document.getElementById("container");
   container.innerHTML = "";
   // طبّق الخطوة 6 تحت هذا الكومنت
+  orders.forEach (    `<div><h1>${item.name} - ${item.id}</h1><h3>${item.order}</h3></div>`
+  )
 
 }
